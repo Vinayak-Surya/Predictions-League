d85753afa16f68c592f9c46c60a782965174195a
Vinayak <vasuryavinu@gmail.com>
2022-01-13 12:03:13 +0530

    html added

--- src/Pages/Rules.js
+++ src/Pages/Rules.js
@@ -3,7 +3,42 @@ import React from "react";
 export default function Rules() {
     return (
         <div>
-            Home
+            {/* <head>
+            <link rel="stylesheet" type="text/css" href="style.css" />
+            <link rel = "icon" href = "../images/logo.png" type = "image/x-icon" />
+            <title>Login</title>
+            </head> */}
+            <div style="background-image: url('../images/pre.jpg'); background-repeat: no-repeat; background-size: cover;">
+                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+                <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+                    <div class="container-fluid">
+                    <a class="navbar-brand" href="https://www.laliga.com/en-GB" target="_blank"> <img src="../images/lal.png" alt="" width="100" class="d-inline-block align-text-top" /> </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="home.html">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="login.html">Login</a>
+                        </li>
+                        </ul>
+                        </div>
+                    </div>
+                </nav>
+                <section class="text-light">
+                    <div class="main my-4">
+                        <div>
+                            <h1 class="text-center display-3">Rules</h1>
+                            <hr class="w-25 mx-auto" />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     )
 }
