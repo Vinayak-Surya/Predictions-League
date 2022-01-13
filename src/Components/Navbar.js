62b31bd0e658769840fd2725082a1ecc6dc8f576
Vinayak <vasuryavinu@gmail.com>
2022-01-13 14:14:38 +0530

    favicon added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -7,19 +7,27 @@ export default function Navbar() {
         <div>
             <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
             <div className="container-fluid">
-                <a className="navbar-brand" href="https://www.laliga.com/en-GB" target="_blank"> <img src={Logo} alt="" width="100" className="d-inline-block align-text-top" /> </a>
+                <Link className="navbar-brand" to="/"> <img src={Logo} alt="" width="100" className="d-inline-block align-text-top" /> </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                    <li className="nav-item">
+                <div className="collapse navbar-collapse d-md-flex justify-content-end" id="navbarSupportedContent">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item mr-3">
                     {/* <a className="nav-link active" aria-current="page" href="rules.html">Rules</a> */}
-                    <Link to="/rules">Rules</Link>
+                    <Link className="text-light" to="/predictions">Predictions</Link>
+                    </li>
+                    <li className="nav-item mr-3">
+                    {/* <a className="nav-link active" aria-current="page" href="rules.html">Rules</a> */}
+                    <Link className="text-light" to="/profile">Profile</Link>
+                    </li>
+                    <li className="nav-item mr-3">
+                    {/* <a className="nav-link active" aria-current="page" href="rules.html">Rules</a> */}
+                    <Link className="text-light" to="/rules">Rules</Link>
                     </li>
                     <li className="nav-item">
                     {/* <a className="nav-link" href="login.html">Login</a> */}
-                    <Link to="/login">Login</Link>
+                    <Link className="text-light" to="/login">Login</Link>
                     </li>
                     </ul>
                 </div>
