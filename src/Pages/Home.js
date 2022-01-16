303b476e2874f9cc7c9240a2d3323c451af11438
sparshsharoff <sparshsharoff123@gmail.com>
2022-01-17 00:40:12 +0530

    added second vid

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,46 +6,44 @@ import Barca from "../Images/s.jpeg";
 import Atletico from "../Images/ts.jpg";
 import Navbar from "../Components/Navbar";
 import Video from "../Images/Video.mp4";
-
+import Video2 from "../Images/Soccer-intro.mp4"
 export default function Home() {
-    return (
+  return (
+    <div>
+      <Navbar />
       <div>
-        <Navbar />
-        <div>
-          <div id="carouselExampleCaptions" className="carousel slide carousel-fade" data-bs-ride="carousel">
-            <div className="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-              <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button>
+        <div id="carouselExampleCaptions" className="carousel slide carousel-fade" data-bs-ride="carousel">
+          <div className="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            {/* <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="3" aria-label="Slide 4"></button> */}
+          </div>
+          <div className="carousel-inner">
+            <div className="carousel-item active" data-bs-interval="13000">
+              <video className="img-fluid d-block" autoplay="autoplay" loop muted>
+                <source src={Video} type="video/mp4" />
+              </video>
             </div>
-            <div className="carousel-inner">
-              <div className="carousel-item active" data-bs-interval="13000">
-                <video className="img-fluid d-block" autoplay="autoplay" loop muted>
-                  <source src={Video} type="video/mp4" />
-                </video>
-                </div>
-              <div className="carousel-item" data-bs-interval="13000">
-                <img src={RealMadrid} className="d-block" alt="..." />
-                <div className="carousel-caption d-md-block">
-                  <h3>Real Madrid</h3>
-                  <h4>Real Madrid winning LaLiga</h4>
-                </div>
+            <div className="carousel-item" data-bs-interval="13000">
+              <video className="img-fluid d-block" autoplay="autoplay" loop muted>
+                <source src={Video2} type="video/mp4" />
+              </video>
+              {/* </div>
+            <div className="carousel-item" data-bs-interval="13000">
+              <img src={RealMadrid} className="d-block" alt="..." />
+              <div className="carousel-caption d-md-block">
+                <h3>Real Madrid</h3>
+                <h4>Real Madrid winning LaLiga</h4>
               </div>
-              <div className="carousel-item" data-bs-interval="13000">
-                <img src={Barca} className="d-block" alt="..." />
-                <div className="carousel-caption d-md-block">
-                  <h3>Barcelona</h3>
-                  <h4>Barcelona winning LaLiga</h4>
-                </div>
+            </div>
+            <div className="carousel-item" data-bs-interval="13000">
+              <img src={Barca} className="d-block" alt="..." />
+              <div className="carousel-caption d-md-block">
+                <h3>Barcelona</h3>
+                <h4>Barcelona winning LaLiga</h4>
               </div>
-              <div className="carousel-item" data-bs-interval="13000">
-                <img src={Atletico} className="d-block" alt="..." />
-                <div className="carousel-caption d-md-block">
-                  <h4>Atletico Madrid</h4>
-                  <h3>Atletico Madrid winning LaLiga</h3>
-                </div>
-              </div>
+            </div> */}
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
               <span className="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -59,15 +57,15 @@ export default function Home() {
         </div>
         <footer>
           <div className="main my-4">
-              <div>
-                  <h1 className="text-center display-3">About</h1>
-                  <hr className="w-25 mx-auto"/>
-              </div>
-          </div>
-            <div className="text-center">
-                <p>Copyright &copy; 2022 My Website</p>
+            <div>
+              <h1 className="text-center display-3">About</h1>
+              <hr className="w-25 mx-auto" />
             </div>
+          </div>
+          <div className="text-center">
+            <p>Copyright &copy; KSV</p>
+          </div>
         </footer>
       </div>
-    )
+      )
 }
